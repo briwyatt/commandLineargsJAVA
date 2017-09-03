c1266fe67ecb648d44914e22fb700e1d81f4bac4
Bri Wyatt <mbriwyatt@gmail.com>
2017-09-02 22:30:19 -0500

    first commit

---
+++ commandlineargdemo.java
@@ -0,0 +1,12 @@
+class Demo{
+     public static void main(String[] args){
+         System.out.println("Argument one = "+args[0]);
+         System.out.println("Argument two = "+args[1]);
+    }
+}
+
+
+// Command Line arguments can be used to specify config info while launching the application
+// you can have as many args as you want
+// the arg information is passed as Strings
+//  & the information is then captured into the String args of your main method
